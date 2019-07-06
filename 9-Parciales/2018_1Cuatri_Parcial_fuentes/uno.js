21cd442a84c518070781e1030fb373f5f77a7744
julisalve <52511592+julisalve@users.noreply.github.com>
2019-07-05 22:05:22 -0300

    parciales

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,15 @@
-
+//Realizar el algoritmo que pida el ancho y el largo de un rectángulo por prompt y// 
+//que muestre el perímetro por alert.//
 function mostrar()
 {
+    var ancho;
+    var largo;
+    var perimetro;
+
+    ancho =prompt("Ingrese el ancho");
+    largo=prompt("Ingrese el largo");
+
+    perimetro=ancho*2+largo*2;
+    alert(perimetro);
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,14 @@
 
 function mostrar()
 {
+    var ancho;
+    var largo;
+    var perimetro;
 
+    ancho=prompt("Ingrese el ancho");
+    largo=prompt("Ingrese el largo");
+
+    perimetro=ancho*2+largo*2;
+
+    alert(perimetro);
 }
