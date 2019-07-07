bfc78d6bfb8aae2f7a8bdaa6cbce92011fdfbc9c
julisalve <52511592+julisalve@users.noreply.github.com>
2019-07-06 21:35:26 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cuatro.js
@@ -1,4 +1,22 @@
 function mostrar()
 {
+    var numeroUno;
+    var numeroDos;
+    var resultado;
+
+    numeroUno=prompt("Indique un numero")
+    numeroDos=prompt("Indique otro numero")
+
+    numeroUno=parseInt(numeroUno);
+    numeroDos=parseInt(numeroDos);
+
+    resultado=numeroUno+numeroDos;
+
+    if(numeroUno==numeroDos)
+    {
+        
+    }
+
+    alert(resultado);
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/tres.js
@@ -1,4 +1,15 @@
+//Pedir por prompt el precio y el porcentaje de descuento, 
+//mostrar el precio final con descuento por id.""
+
 function mostrar()
 {
+    var precio;
+    var porcentajeDeDescuento;
+    var precioFinalConDescuento;
 
+    precio=prompt("Indique el precio");
+    porcentajeDeDescuento=prompt("Indique porcentaje de descuento");
+    precioFinalConDescuento=precio-precio*porcentajeDeDescuento/100;
+
+    document.getElementById("elPrecioFinal").value=precioFinalConDescuento;
 }
