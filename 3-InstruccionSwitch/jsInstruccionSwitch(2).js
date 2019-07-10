3148d1747bad775ab7b63905495e00f456da734c
julisalve <52511592+julisalve@users.noreply.github.com>
2019-07-10 20:32:51 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,9 +1,75 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelAño 
+    mesDelAño= document.getElementById('mes').value;
 
-alert (mesDelAño);
+    switch(mesDelAño)
+    {
+        case "Enero":
+            {
+                alert("Falta para el invierno");
+                break;
+            }
+        
+        case "Febrero":
+            {
+                alert("Falta para el invierno");
+                break;
+            }
+        case "Marzo":
+            {
+                alert("Falta para el invierno");
+                break;
+            }
+        case "Abril":
+            {
+                alert("Falta para el invierno");
+                break;
+            }
+        case "Mayo":
+            {
+                alert("Falta para el invierno");
+                break;
+            }
+        case "Junio":
+            {
+                alert("Falta para el invierno");
+                break;
+            }
+        case "Julio":
+            {
+                alert("Abrigate que hace frio");
+                break;
+            }
+        case "Agosto":
+            {
+                alert("Abrigate qeu hace frio");
+                break;
+            }
+        case "Septiembre":
+            {
+                alert("Ya pasamo el invierno, ahora calor");
+                break;
+            }
+        case "Octubre":
+            {
+                alert("Ya pasamos el invierno, ahora calor");
+                break;
+            }
+        case "Noviembre":
+            {
+                alert("Ya psamos el invierno, ahora el calor");
+                break;
+            }
+        case "Diciembre":
+            {
+                alert("Ya pasamos el invierno, ahora el calor");
+                break;
+            }
+}
+
+
 
 
 
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,7 +1,74 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+var mesDelAño 
+
+    mesDelAño= document.getElementById('mes').value;
+
+    switch(mesDelAño)
+    {
+        case "Enero":
+            {
+                alert("Este mes tiene 30 o mas dias");
+                break;
+            }
+        
+        case "Febrero":
+            {
+                alert("Este mes no tiene mas de 29 dias");
+                break;
+            }
+        case "Marzo":
+            {
+                alert("Este mes tiene 30 o mas dias");
+                break;
+            }
+        case "Abril":
+            {
+                alert("Este mes tiene 30 o mas dias");
+                break;
+            }
+        case "Mayo":
+            {
+                alert("Este mes tiene 30 o mas dias");
+                break;
+            }
+        case "Junio":
+            {
+                alert("Este mes tiene 30 o mas dias");
+                break;
+            }
+        case "Julio":
+            {
+                alert("Este mes tiene 30 o mas dias");
+                break;
+            }
+        case "Agosto":
+            {
+                alert("Este mes tiene 30 o mas dias");
+                break;
+            }
+        case "Septiembre":
+            {
+                alert("Este mes tiene 30 o mas dias");
+                break;
+            }
+        case "Octubre":
+            {
+                alert("Este mes tiene 30 o mas dias");
+                break;
+            }
+        case "Noviembre":
+            {
+                alert("Este mes tiene 30 o mas dias");
+                break;
+            }
+        case "Diciembre":
+            {
+                alert("Este mes tiene 30 o mas dias");
+                break;
+            }
+        }
 
 //alert (mesDelAño);
 	
