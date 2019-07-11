5e041ab392c952613377fe0dc3c6fe01ebcaae08
julisalve <52511592+julisalve@users.noreply.github.com>
2019-07-10 22:17:26 -0300

    probar iluminacion

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -13,25 +13,67 @@ function CalcularPrecio ()
      var cantidad;
      var precio;
      var precioFinal;
+     var marca;
+     var iibb;
+
 
      cantidad=document.getElementById("Cantidad").value;
      cantidad=parseInt(cantidad);
 
+     marca=document.getElementById("Marca").value;
+     
      precio=35;
 
+     //A//
      if(cantidad>=6)
      {
         precioFinal=(precio*cantidad)*50/100;
-        precioFinal=parseInt(precioFinal); 
         document.getElementById("precioDescuento").value=precioFinal;
            
      }
-     else
+     else if(cantidad==5 && marca=="ArgentinaLuz")
      {
-         precioFinal=precio*cantidad;
-         precioFinal=parseInt(precioFinal);
+         precioFinal=(precio*cantidad)*60/100 //40% de dto
          document.getElementById("precioDescuento").value=precioFinal;
-         
+     }
+     else if (cantida==5 && marca !="ArgentinaLuz")
+     {
+         precioFinal=(precio*cantidad)*70/100; //30% de dto
+         document.getElementById("precioDescuento").value=precioFinal;
+
+     }
+
+     else if(cantidad==4 && (marca=="ArgetinaLuz" || marca=="FelipeLamparas"))
+     {
+         precioFinal=(precio*cantidad)*0.75; //25% de dto
+         document.getElementById("precioDescuento").value=precioFinal;
+     }
+     else if (cantidad==4 && marca!="ArgentinaLuz" && marca!="FelipeLamparas")
+     {
+         precioFinal=(precio*cantidad)*0.8; //20% de dto
+         document.getElementById("precioDescuento").value=precioFinal;
+     }
+     else if (cantidad==3 && marca=="ArgentinaLuz")
+     {
+         precioFinal=(precio*cantidad)*0.85;
+         document.getElementById("precioDescuento").value=precioFinal;
+     }
+     else if (cantidad==3 && marca=="FelipeLamparas")
+     {
+         precioFinal=(precio*cantidad)*0.9;
+         document.getElementById("precioDescuento").value=precioFinal;
+     }
+     else if(cantidad==3 && marca!="ArgentinaLuz" && marca!="FelipeLamparas")
+     {
+         precioFinal=(precio*cantidad)*0.95;
+         document.getElementById("precioDescuento").value=precioFinal;
+     }
+     else if(precioFinal>120)
+     {
+        iibb=precioFinal*0.1
+        precioFinal=precioFinal+iibb;
+        document.getElementById("precioDescuento").value=precioFinal;
+        alert("IIBB Usted paga "+iibb);
      }
 
 
