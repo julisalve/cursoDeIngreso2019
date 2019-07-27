845ce23a40343b87dd089ecebeb82e91cfe43cfa
julisalve <52511592+julisalve@users.noreply.github.com>
2019-07-27 18:46:53 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,90 @@
 function mostrar()
 {
+var letra;
+var numero;
+var respuesta="si";
+var cantidadNumerosPares=0;
+var cantidadNumerosImpares=0;
+var cantidadNumeroCero=0;
+var numeroPositivo=0;
+var acumuladorNumerosPos=0;
+var numeroNegativo=0;
+var numeroMenor=0;
+var numeroMayor;
+var promedio;
+var acumulador=0;
+var contador=0;
+var letraMenor;
+
+while(respuesta=="si")
+{
+    numero=prompt("Indique un numero");
+    numero=parseInt(numero);
+
+    while(isNaN(numero)==true || numero<-100 || numero>100)
+
+    {
+        numero=prompt("No es un dato valido. Ingrese nuevamente otro numero entre -100 y 100");
+        numero=parseInt(numero)
+    }
+
+    letra=prompt("Ingrese una letra");
+
+    while(isNaN(letra)==false)
+    {
+        letra=prompt("No es una letra, Ingrese nuevamente el dato");
+    }
+
+    if(numero%2==0 && numero!=0)
+    {
+        cantidadNumerosPares++;
+    }
+
+    else
+    {
+        if(numero==0)
+        {
+            cantidadNumeroCero++
+        }
+        else
+        {
+            cantidadNumerosImpares++;
+        }
+        
+    }
+
+    if(numero>0)
+    {
+        numeroPositivo=numeroPositivo+numero;
+        acumuladorNumerosPos++;
+    }
+
+    else
+    {
+        numeroNegativo=numeroNegativo+numero;
+    }
+
+    respuesta=prompt("Quiere seguir cargando otro dato?");
+
+    if(contador==0 || numero<numeroMenor)
+    {
+        numeroMenor=numero;
+        letraMenor=letra;
+    }
+    else
+    { 
+        numeroMayor
+    }
+
+
+}
+
+promedio=numeroPositivo/acumuladorNumerosPos;
+if(isNaN(promedio)==true)
+{
+    promedio="No hay numeros pares";
+}
+document.write("A)La cantidad de numeros pares es: "+cantidadNumerosPares+"<br>"+"B) La cantidad de numero impares es: "+cantidadNumerosImpares+"<br>"+"C) La cantidad de numeros cero es: "+cantidadNumeroCero+"<br>"+"D) El promedio de numeros pares es :"+ promedio+ "<br>"+ "E) La suma de numeros negativos es: "+numeroNegativo)
+
 
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -6,6 +6,9 @@ function mostrar()
     var contador;
     var sumaDeNotas;
     var promedio;
+    var notaMenor=0;
+    var sexoNotaMenor;
+    var cantidad=0;
 
 
     sumaDeNotas=0;
@@ -19,13 +22,13 @@ function mostrar()
         nota=prompt("Indique nota");
         nota=parseInt(nota);
 
-        while(nota<0 || nota>10)
+        while(isNaN(nota)==true || nota<0 || nota>10)
         {
-            nota=prompt("La nota no esta dentro del rango correcto. Ingrese una nueva nota");
+            nota=prompt("La nota no esta dentro del rango correcto o no es un valor numerico. Ingrese un valor correcto entre 0 y 10");
             nota=parseInt(nota);
             
         }
-    
+
         sexo=prompt("Indique sexo");
 
         while(sexo!="m" && sexo!="f")
@@ -33,19 +36,27 @@ function mostrar()
             sexo=prompt("El sexo no es valido."+"\n"+ "Ingrese un nuevo valor, el cual puede ser m para masculino o f para femenino");
         }
 
-        
         sumaDeNotas=sumaDeNotas+nota;
 
-        promedio=sumaDeNotas/contador;
+        
 
-
-
+        if(contador==0 || nota<notaMenor)
+        {
+            notaMenor=nota;
+            sexoNotaMenor=sexo;
+        }
+        
+        if(sexo=="m" && nota>=6)
+        {
+            cantidad=cantidad+1;
+        }
 
         
-        
+         
     }
 
+    promedio=sumaDeNotas/contador;
 
-    alert("El promedio de todas las notas es "+promedio);
+    alert("El promedio de todas las notas es "+promedio+"\n"+"La nota mas baja es "+notaMenor+" y el sexo es "+sexoNotaMenor+"\n"+"La cantidad de varones que su nota fue 6 o mas es: "+cantidad);
 
 }
