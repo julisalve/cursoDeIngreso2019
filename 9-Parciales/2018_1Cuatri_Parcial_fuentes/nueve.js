c67bb098e57e47a49ee308bfab17cd8dec7adb72
julisalve <52511592+julisalve@users.noreply.github.com>
2019-07-31 22:04:40 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -6,10 +6,14 @@ function mostrar()
     var respuesta="si";
     var cantTempPares=0;
     var marcaMasPesada;
-    var productosMenosCeroGrrados;
+    var productosMenosCeroGrados=0;
     var promedio;
+    var acumuladorPeso=0;
     var pesoMax;
     var pesoMin;
+    var contador=0;
+    var marcaMasPesada;
+
 
     while(respuesta=="si")
     {
@@ -21,7 +25,22 @@ function mostrar()
         {
             peso=prompt("Los valores indicados no son numericos ni se encuentran dentro del rango entre 1 y 100."+"\n"+ "Ingrese nuevo valor");
             peso=parseInt(peso);
+
         }
+        if(contador==0)
+        {
+            pesoMax=peso;
+            marcaMasPesada=marca;
+            
+        }
+        else if (peso>pesoMax)
+        {
+           pesoMax=peso;
+            marcaMasPesada=marca;
+        }
+        contador++;
+        acumuladorPeso=acumuladorPeso+peso;
+
 
         temperatura=prompt("Indique temperatura del producto");
         temperatura=parseInt(temperatura);
@@ -30,15 +49,34 @@ function mostrar()
         {
             temperatura=prompt("Indique temperatura valida entre -30 y 30 grados");
             temperatura=parseInt(temperatura);
+
+        }
+        if(temperatura%2==0)
+            {
+            cantTempPares++;   
+            }
+
+        if(temperatura<0)
+        {
+            productosMenosCeroGrados++;
+        }
+        else
+        {
+            productosMenosCeroGrados="No hay productos que se conserven a menos de cero grados";
         }
 
         respuesta=prompt("Quiere seguir cargando mas datos?");
+    
+        promedio=acumuladorPeso/contador;
     }
 
-    if(temperatura%2==0)
-    {
-     cantTempPares++;   
-    }
+    
 
-    document.write("A)La cant de temperaturas pares es: "+cantTempPares)
+
+
+
+    document.write("A)La cant de temperaturas pares es: "+cantTempPares+"<br>")
+    document.write("B) La marca mas pesada es: "+marcaMasPesada+"<br>")
+    document.write("C) La cantidad de producto que se conserva a menos de 0 grados es: "+ productosMenosCeroGrados+"<br>") 
+    document.write("D) El promedio del peso de todos los productos es: "+ promedio)
 }
